192bb63886ea40666932e6bc51ab37a50512a794
SAMIRSAWALE <samirsawale2002@gmail.com>
2026-02-15 14:16:53 +0530

    feat/updated

--- src/component/restroMenu.jsx
+++ src/component/restroMenu.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-
 import { useParams } from "react-router";
-
 import ShimmerUi from "./shimmer";
 import { usefetchDataCustomHook } from "../utils/api";
 import RestCatTitle from "./resCatTitle";
